9a6f2f21f634bfe96d43f4b0a0c0b1886b29339b
Hagjdkd <villarinoemman030@gmail.com>
2026-03-25 21:45:42 +0800

    Second Commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,37 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('IntegrativeProgramming');
+  username = '';
+  password = '';
+  message = '';
+
+  validatePassword(pass: string): boolean {
+    const upper = /[A-Z]/;
+    const lower = /[a-z]/;
+    const number = /[0-9]/;
+
+    return upper.test(pass) && lower.test(pass) && number.test(pass);
+  }
+
+  login() {
+    if (this.username.trim() === '') {
+      this.message = 'Please enter your username.';
+      return;
+    }
+
+    if (!this.validatePassword(this.password)) {
+      this.message = 'Password must contain uppercase, lowercase, and number!';
+      return;
+    }
+
+    this.message = `Welcome to Barangay Information & Services System, ${this.username}!`;
+  }
 }
