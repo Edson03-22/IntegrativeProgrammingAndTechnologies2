0e02e02ffc8db59362858999d76e9e8d7ddaa78a
Hagjdkd <villarinoemman030@gmail.com>
2026-04-13 19:12:43 +0800

    Activity8

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,6 @@
 import { Component, inject, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { RouterOutlet, Router } from '@angular/router';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -12,29 +12,5 @@ import { RouterOutlet, Router } from '@angular/router';
 export class App {
   protected readonly title = signal('IntegrativeProgramming');
   
-  // Changed name to 'router' to match your login function
-  private router = inject(Router);
-  
-  username = '';
-  password = '';
-  message = ''; // Added this to fix the HTML error
-  isLoggedin = false;
 
-  validatePassword(pass: string): boolean {
-    const upper = /[A-Z]/;
-    const lower = /[a-z]/;
-    const number = /[0-9]/;
-    return upper.test(pass) && lower.test(pass) && number.test(pass);
-  }
-
-  login() {
-  if (this.username === 'admin' && this.password === 'admin123') {
-    this.message = 'Login successful! Redirecting...';
-    
-    // The string here must match the 'path' in app.routes.ts exactly
-    this.router.navigate(['/dashboard']); 
-  } else {
-    this.message = 'Invalid username or password.';
-  }
-}
 }
